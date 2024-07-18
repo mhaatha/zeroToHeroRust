da3c68c6ae354bdcff9ffe53f27565b07872b1f0
Hafidz Athaya <mhafidza528@gmail.com>
2024-07-18 22:42:44 +0800

    number data type and integer overflow

--- belajar-rust-dasar/src/main.rs
+++ belajar-rust-dasar/src/main.rs
@@ -50,3 +50,30 @@ fn explicit() {
     let age: i32 = 19;
     println!("{}", age);
 }
+
+#[test]
+fn number() {
+    let a: i8 = 10;
+    println!("{}", a);
+
+    let b: f32 = 10.5;
+    println!("{}", b);
+}
+
+#[test]
+fn number_convertion() {
+    let a: i8 = 10;
+    println!("{}", a);
+
+    let b: i16 = a as i16;
+    println!("{}", b);
+
+    let c: i32 = b as i32;
+    println!("{}", c);
+
+    let d: i64 = 1000000000;
+    println!("{}", d);
+
+    let e: i8 = d as i8; // Integer Overflow
+    println!("{}", e);
+}
